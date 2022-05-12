a52cf34e8385d37ecd03d622d9ce243e42901f26
Jarek <j.szczepaniak@albedomarketing.pl>
2022-05-12 08:35:25 +0200

    Add error handling.

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, {json} from 'express';
 import cors from 'cors';
 import 'express-async-errors';
+import {handleError} from "./utils/errors";
 
 const app = express();
 
@@ -9,6 +10,14 @@ app.use(cors({
 }))
 
 app.use(json());
+
+// Routes
+
+// app.get('/', async (req, res) => {
+//     throw new Error('ołłłł');
+// })
+
+app.use(handleError);
 
 app.listen(3001, '0.0.0.0', () => {
     console.log('Listening on port http://localhost:3001')
