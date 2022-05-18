74ee94fe3954d6498e9be86f964c4451e29fbb1b
Jarek <j.szczepaniak@albedomarketing.pl>
2022-05-18 11:53:48 +0200

    Added searching and listing of ads.

--- tests/ad-record.test.ts
+++ tests/ad-record.test.ts
@@ -1,24 +1,24 @@
 import {AdRecord} from "../records/ad.records";
 
 const defaultObj = {
-    name: 'test name',
-    description: 'asdasd',
+    name: 'Test Name',
+    description: 'blah',
     url: 'https://megak.pl',
     price: 0,
     lat: 9,
-    lon: 9
-}
+    lon: 9,
+};
 
-test('Can build adRecord', () => {
-    const ad = new AdRecord(defaultObj)
+test('Can build AdRecord', () => {
+    const ad = new AdRecord(defaultObj);
 
-    expect(ad.name).toBe('test name');
-    expect(ad.description).toBe('asdasd');
-})
+    expect(ad.name).toBe('Test Name');
+    expect(ad.description).toBe('blah');
+});
 
 test('Validates invalid price', () => {
     expect(() => new AdRecord({
         ...defaultObj,
-        price: -3
-    })).toThrow('Cena nie może być mniejsza niż 0 lub większa niż 9 999 999`');
-})
+        price: -3,
+    })).toThrow('Cena nie może być mniejsza niż 0 lub większa niż 9 999 999')
+});
--- tests/ad.test.ts
+++ tests/ad.test.ts
@@ -1,4 +1,10 @@
 import {AdRecord} from "../records/ad.records";
+import {pool} from "../utils/db";
+import {AdEntity} from "../types";
+
+afterAll(async () => {
+    await pool.end();
+})
 
 test('Adrecord returns data from db for one entry', async () => {
     const ad = await AdRecord.getOne('abc');
@@ -10,8 +16,36 @@ test('Adrecord returns data from db for one entry', async () => {
     expect(ad.name).toBe('testowe')
 })
 
-test('AdRecord returns null from db for unexisting entry', async () => {
+test('AdRecord.getOne returns null from db for unexisting entry', async () => {
     const ad = await AdRecord.getOne('---');
 
     expect(ad).toBeNull();
+})
+
+test('AdRecord.findAll returns array of found entries.', async () => {
+    const ads = await AdRecord.findAll('');
+
+    expect(ads).not.toEqual([]);
+    expect(ads[0].id).toBeDefined();
+})
+
+test('AdRecord.findAll returns array of found entries when searching for "e".', async () => {
+    const ads = await AdRecord.findAll('e');
+
+    expect(ads).not.toEqual([]);
+    expect(ads[0].id).toBeDefined();
+})
+
+test('AdRecord.findAll returns empty array when searching for sth that does not exist.', async () => {
+    const ads = await AdRecord.findAll('-----------------------------------------');
+
+    expect(ads).toEqual([]);
+})
+
+test('AdRecord.findAll returns smaller amount of data.', async () => {
+    const ads = await AdRecord.findAll('');
+
+    // żeby poczitować to można dać as AdEntity, żeby miało typ w którym są wszystkie wartości
+    expect((ads[0] as AdEntity).price).toBeUndefined();
+    expect((ads[0] as AdEntity).description).toBeUndefined();
 })
